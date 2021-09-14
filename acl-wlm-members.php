ed01c66aa041f77cbe29364c7143ce48db9ef4c9
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-09-13 15:39:42 -1000

    Update acl-wlm-members.php

--- acl-wlm-members.php
+++ acl-wlm-members.php
@@ -14,8 +14,8 @@ Version 1.0 - Original Version
 
 function acl_get_wlmopts( $atts, $content ) {
 	$members = wlmapi_the_level_members(1631561301);
-	$memmore = $members['_more_');
-	foreach ($memmore as $k=>$v) {
+	// $memmore = $members['_more_');
+	foreach ($members as $k=>$v) {
 		echo 'Key: '.$k.'<br />';
 		var_dump ($v);
 		echo '<br />';
