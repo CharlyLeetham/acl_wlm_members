01b0aed1ecee72635efca2fb64fa401412594ac8
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-09-16 11:48:39 -1000

    Update acl-wlm-members.php

--- acl-wlm-members.php
+++ acl-wlm-members.php
@@ -158,7 +158,7 @@ function acl_get_wlmopts( $atts, $content ) {
 	// echo '</div>';
 	
 	
-	/*echo '<style>
+	echo '<style>
 			.newwrapper {
 			  border: 1px solid grey;
 			  display: flex;
@@ -192,7 +192,7 @@ function acl_get_wlmopts( $atts, $content ) {
 	  <div class="c">c</div>
 	  <div class="d">d</div>
 	</div>';
-	*/
+
 
 
 
