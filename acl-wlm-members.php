0642f785637a7f9db308d84bc0afdce3875f151e
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-09-15 16:46:06 -1000

    Update acl-wlm-members.php

--- acl-wlm-members.php
+++ acl-wlm-members.php
@@ -107,14 +107,14 @@ function acl_get_wlmopts( $atts, $content ) {
 			<div class="column details"><div class="item">Details</div></div>
 			<div class="column approve"><div class="item">Approve</div></div>
 		</header> <!-- row -->
-		<!-- <article class="main">
+		<article class="main">
 			<div class="row"><div class="item">Email address</div></div>
 			<div class="row"><div class="item">Faculty</div></div>
 			<div class="row"><div class="item">Department</div></div>
 			<div class="row"><div class="item">Dissertation Defence</div></div>
 		</article>
 		<aside class="aside aside-1"><div class="item">Full Name</div></aside>
-		<aside class="aside aside-2"><div class="item">Approve</div></aside>		-->';
+		<aside class="aside aside-2"><div class="item">Approve</div></aside>';
 
 	
 	foreach ( $approvids as $k => $v ) {
@@ -122,7 +122,7 @@ function acl_get_wlmopts( $atts, $content ) {
 		$user_info = get_userdata($v); // Get the user info so we can get First and Last Name
 		$memdata = $approvmem['member'][0]['UserInfo']['wldata'];
 		echo ' 
-		<article class="main">
+		<!-- <article class="main">
 			<div class="row">			
 				<div class="item">'.$approvmem['member'][0]['UserInfo']['user_email'].'</div>
 			</div>
@@ -154,7 +154,7 @@ function acl_get_wlmopts( $atts, $content ) {
 		</aside>
 		<aside class="aside aside-2"?
 			<div class="column "><p class="item">chk</p></div>
-		</aside>';
+		</aside>-->';
 	};
 	
 	echo '</div>';
