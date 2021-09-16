f2db3c30d0aa6ac9bf0513b4b8405ab70fea51ef
Charly Leetham <helpdesk@askcharlyleetham.com>
2021-09-16 13:44:26 -1000

    Update acl-wlm-members.php

--- acl-wlm-members.php
+++ acl-wlm-members.php
@@ -231,11 +231,11 @@ function acl_get_wlmopts( $atts, $content ) {
 	  <div class="ad">Advertising</div>
 	  
 	  <article class="content">
-		<h1>Main article area</h1>
-		<p>In this layout, we display the areas in source order for any screen less that 500 pixels wide. We go to a two column layout, and then to a three column layout by redefining the grid, and the placement of items on the grid.</p>
+		<h1>Main article area2</h1>
+		<p>In this layout, we dddddisplay the areas in source order for any screen less that 500 pixels wide. We go to a two column layout, and then to a three column layout by redefining the grid, and the placement of items on the grid.</p>
 	  </article>
-	  <aside class="side">Sidebar</aside>
-	  <div class="ad">Advertising</div>	  
+	  <aside class="side">Sidebar2</aside>
+	  <div class="ad">Advertising444</div>	  
 	  <footer class="main-footer">The footer</footer>
 	</div>
 	';
